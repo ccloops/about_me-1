e0f6393a7f4947b578fc4cbe4daec60952f0f9c5
birdie <birdiedev@gmail.com>
2017-09-19 16:09:20 -0700

    added some comments for clarification on the different questions and responses

--- js/app.js
+++ js/app.js
@@ -9,6 +9,7 @@ alert('Hi, ' + userName + '! Let\'s play a guessing game! Please guess YES or NO
 // Question One - Trombone Question
 var tromboneGuess = prompt('Question 1: Birdie can play the trombone? y/n').toLowerCase();
 console.log('#1 - Trombone question answer is "n" or "no". ' + userName + ' entered ' + tromboneGuess);
+// Trombone question responses to user
 if (tromboneGuess === 'n' || tromboneGuess === 'no') {
   alert('You are correct! Birdie can\'t play the trombone although she (sort of) can play the clarinet.');
 } else {
@@ -18,6 +19,7 @@ if (tromboneGuess === 'n' || tromboneGuess === 'no') {
 // Question Two - Dog Question
 var dogNameGuess = prompt('Question 2: Birdie has a dog named Rosie. y/n').toLowerCase();
 console.log('#2 - Dog question answer is "n" or "no". ' + userName + ' entered ' + dogNameGuess);
+// Dog question responses to user
 if (dogNameGuess === 'no' || dogNameGuess === 'n') {
   alert('Ah, yes! Good job, ' + userName + '! Birdie\'s dog is named Lotus.');
 } else {
@@ -27,6 +29,7 @@ if (dogNameGuess === 'no' || dogNameGuess === 'n') {
 // Question Three - Flight Question
 var flightGuess = prompt('Question 3: Birdie can really fly? y/n').toLowerCase();
 console.log('#3 - Flight question answer is "y" or "yes". ' + userName + ' entered ' + flightGuess);
+// Flight question responses to user
 if (flightGuess === 'y' || flightGuess === 'yes') {
   alert('Correct! Why not?!');
 } else if (flightGuess === 'maybe') {
@@ -39,6 +42,7 @@ else {
 // Question Four - Unicorn Question
 var unicornGuess = prompt('Question 4: Birdie is part unicorn? y/n').toLowerCase();
 console.log('#4 - Unicorn question answer is "n" or "n". ' + userName + ' entered ' + flightGuess);
+// Unicorn question responses to user
 if (unicornGuess === 'y' || unicornGuess === 'yes') {
   alert('Incorrect. Birdie is not part unicorn. But maybe... It was a good guess, ' + userName + '.');
 } else {
@@ -48,6 +52,7 @@ if (unicornGuess === 'y' || unicornGuess === 'yes') {
 // Question Five - Simulation Question
 var simulationGuess = prompt('Question 5: We are all living in a simulation? y/n').toLowerCase();
 console.log('#5 - Simulation question answer is "y" or "yes". ' + userName + ' entered ' + simulationGuess);
+// Simulation question responses to user
 if (simulationGuess === 'n' || simulationGuess === 'no') {
   alert('Hmmmm... ' + userName + '. I have no idea but it is very interesting that you think not.');
 } else {
@@ -55,17 +60,3 @@ if (simulationGuess === 'n' || simulationGuess === 'no') {
 }
 
 alert(userName + '! That was so fun! I hope you enjoyed playing! Have a wonderful day!');
-
-
-
-
-
-
-// Question 1 Example
-// var answer1 = prompt('Do I have 4 cats? y/n');
-//
-// if (answer1 === 'n') {
-//   alert('You are correct');
-// } else {
-//   alert('You are incorrect');
-// }
