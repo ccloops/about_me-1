0ab6f2ecc4c784638c4b49f6c897784d6dd2b192
Catherine Looper <loopercat@gmail.com>
2017-09-21 14:55:47 -0700

    Added a function to Question 7

--- js/app.js
+++ js/app.js
@@ -75,85 +75,87 @@ function simulationQuestion() {
 simulationQuestion();
 
 // Question Six - Dexterity Question (Number guessing game)
-alert('Ok ' + userName + '... now we are going to mix it up! In the next game, we\'ll roll a D20 to do a dexterity check on you. \n\nI\'ll roll the D20 behind the computer so you can\'t see. I need you to guess your level (random number between 1 and 20) in FOUR tries or less so we can win!! I know you can do it!');
-var userDexterityRoll = Math.floor(Math.random() * 20) + 1;
-console.log('#6 - Dexterity number guessing game. Number rolled is ' + userDexterityRoll);
-var tries = 3;
-while (true) {
-  var userGuessedRoll = Number(prompt(userName + ', what is your guess (between 1 - 20)? *fingers crossed*'));
-  console.log(userName + ' guessed ' + userGuessedRoll)
-  if (userGuessedRoll === userDexterityRoll) {
-    alert('OMG we did it! Thank you ' + userName + '!\nWe can now move on to the next game!!!');
-    console.log(userName + ' won the number game by correctly answering ' + userDexterityRoll);
-    break;
-  } else if (tries === 0) {
+function dexterityQuestion() {
+  alert('Ok ' + userName + '... now we are going to mix it up! In the next game, we\'ll roll a D20 to do a dexterity check on you. \n\nI\'ll roll the D20 behind the computer so you can\'t see. I need you to guess your level (random number between 1 and 20) in FOUR tries or less so we can win!! I know you can do it!');
+  var userDexterityRoll = Math.floor(Math.random() * 20) + 1;
+  console.log('#6 - Dexterity number guessing game. Number rolled is ' + userDexterityRoll);
+  var tries = 3;
+  while (true) {
+    var userGuessedRoll = Number(prompt(userName + ', what is your guess (between 1 - 20)? *fingers crossed*'));
+    console.log(userName + ' guessed ' + userGuessedRoll);
+    if (userGuessedRoll === userDexterityRoll) {
+      alert('OMG we did it! Thank you ' + userName + '!\nWe can now move on to the next game!!!');
+      console.log(userName + ' won the number game by correctly answering ' + userDexterityRoll);
+      break;
+    } else if (tries === 0) {
       alert('Whomp whomp... better luck next time. Your dexterity is ' + userDexterityRoll + '. So close!');
       break;
-  } else if (tries === 1 && userGuessedRoll > userDexterityRoll) {
+    } else if (tries === 1 && userGuessedRoll > userDexterityRoll) {
       alert('Oh no! ' + userName + ', ' + userGuessedRoll + ' is too high! Please try again.. We only have one guess left!');
       tries--;
-  } else if (tries === 1 && userGuessedRoll < userDexterityRoll) {
+    } else if (tries === 1 && userGuessedRoll < userDexterityRoll) {
       alert('Oh no! ' + userName + ', ' + userGuessedRoll + ' is too low! Please try again.. We only have one guess left!');
       tries--;
-  } else if (userGuessedRoll > userDexterityRoll) {
+    } else if (userGuessedRoll > userDexterityRoll) {
       alert('Oh no! ' + userName + ', ' + userGuessedRoll + ' is too high! Please try again.. We only have ' + tries + ' guesses left!');
       tries--;
-  } else if (userGuessedRoll < userDexterityRoll) {
+    } else if (userGuessedRoll < userDexterityRoll) {
       alert('Oh no! ' + userName + ', ' + userGuessedRoll + ' is too low! Please try again.. We only have ' + tries + ' guesses left!');
       tries--;
     }
   }
+}
+dexterityQuestion();
 
   // As a developer, I want to add a seventh question to my guessing game that has multiple possible correct answers that are stored in an array. For instance, "Can you guess a state that I have lived in besides Washington?", so that even more of my programming skills are showcased. For now, I will structure this question so that the user has six tries to get a single correct answer, and upon using up those tries OR getting a correct answer, displays a message to the user indicating all of the possible correct answers.
 // As a developer, I want to keep a tally of the total number of correct answers given by the user, and at the end of the game, tell the user how well they did with a personalized custom message that includes the number of correct answers and also addresses the user by name, e.g. "You got 4 out of 7 questions correct, Bobbi! Better luck next time."
 
 // // Question Seven - Lotus' favorite animals guessing game
-
-//Current data on favorite animals
-var lotusFaveAnimals = ['narwhal', 'penguin', 'sloth', 'owl', 'mouse', 'orca', 'dolphin', 'dragon', 'fox', 'snail'];
-console.log('#7 - Favorite animal guessing game. Lotus has a total of ' + lotusFaveAnimals.length + ' animals. This list contains the following animals: ');
-for (var i = 0; i < lotusFaveAnimals.length; i++) {
-  console.log(lotusFaveAnimals[i] + ' ');
-}
-
-alert('Alright next game!\n\nLotus is a huge animal lover. I need you to guess an animal that is on her favorites list in six gueses or less.\n\nCurrently, Lotus has a total of ' + lotusFaveAnimals.length + ' SUPER favorites!\n\nLet\'s start!');
-
-// As a developer, I want to add a seventh question to my guessing game that has multiple possible correct answers that are stored in an array. For instance, "Can you guess a state that I have lived in besides Washington?", so that even more of my programming skills are showcased. For now, I will structure this question so that the user has six tries to get a single correct answer, and upon using up those tries OR getting a correct answer, displays a message to the user indicating all of the possible correct answers.
-// As a developer, I want to keep a tally of the total number of correct answers given by the user, and at the end of the game, tell the user how well they did with a personalized custom message that includes the number of correct answers and also addresses the user by name, e.g. "You got 4 out of 7 questions correct, Bobbi! Better luck next time."
-
-// psuedo code - Can you guess Lotus' one of lotus' favorite animals
-// Lotus has list of animals
-// user guesses an animals
-// animal is compared against list of favorite animals
-// if correct then end and displays
-// if incorrect lose guess
-
-var userAnimalGuess = prompt('Ok, guess an animal');
-
-for (var j = 0; j < lotusFaveAnimals; j++) {
-  if (userAnimalGuess === lotusFaveAnimals[j]) {
-    alert('Hurray! You did it! Yes, ' + userAnimalGuess + ' is in the list of favorite animals\n\nThe complete list is ');
-  } else {
-    continue;
+function favoriteAnimals() {
+  //Current data on favorite animals
+  var lotusFaveAnimals = ['narwhal', 'penguin', 'sloth', 'owl', 'mouse', 'orca', 'dolphin', 'dragon', 'fox', 'snail'];
+  console.log('#7 - Favorite animal guessing game. Lotus has a total of ' + lotusFaveAnimals.length + ' animals. This list contains the following animals: ');
+  for (var i = 0; i < lotusFaveAnimals.length; i++) {
+    console.log(lotusFaveAnimals[i] + ' ');
   }
+
+  alert('Alright next game!\n\nLotus is a huge animal lover. I need you to guess an animal that is on her favorites list in six gueses or less.\n\nCurrently, Lotus has a total of ' + lotusFaveAnimals.length + ' SUPER favorites!\n\nLet\'s start!');
+
+  // As a developer, I want to add a seventh question to my guessing game that has multiple possible correct answers that are stored in an array. For instance, "Can you guess a state that I have lived in besides Washington?", so that even more of my programming skills are showcased. For now, I will structure this question so that the user has six tries to get a single correct answer, and upon using up those tries OR getting a correct answer, displays a message to the user indicating all of the possible correct answers.
+  // As a developer, I want to keep a tally of the total number of correct answers given by the user, and at the end of the game, tell the user how well they did with a personalized custom message that includes the number of correct answers and also addresses the user by name, e.g. "You got 4 out of 7 questions correct, Bobbi! Better luck next time."
+
+  // psuedo code - Can you guess Lotus' one of lotus' favorite animals
+  // Lotus has list of animals
+  // user guesses an animals
+  // animal is compared against list of favorite animals
+  // if correct then end and displays
+  // if incorrect lose guess
+
+  var userAnimalGuess = prompt('Ok, guess an animal');
+
+  for (var j = 0; j < lotusFaveAnimals; j++) {
+    if (userAnimalGuess === lotusFaveAnimals[j]) {
+      alert('Hurray! You did it! Yes, ' + userAnimalGuess + ' is in the list of favorite animals\n\nThe complete list is ');
+    } else {
+      continue;
+    }
+  }
+  // var animalGuessCount;
+  // var faveAnimalPoints = 0
+  // var faveAnimalFromList;
+  // var correctAnswers = 0
+  // for (animalGuessCount = 6; animalGuessCount > 0; animalGuessCount--) {
+  //   var userAnimalGuess = prompt('Please take a guess at one of Lotus\'s favorite animals.\n\nWe currently have ' + animalGuessCount + ' guesses and ' + faveAnimalPoints + ' points!');
+  //     if (userAnimalGuess === lotusFaveAnimals[1]) {
+  //       alert('Wow ' + userName + '! You got one!\n\nYou have ' + correctAnswers.toString() + ' out of ' + lotusFaveAnimals.length.toString() + '!');
+  //     }
+  //   }
+  //
+  // while (animalGuessCount > 0 || correctAnswers === 1) {
+  //
+  // }
 }
-
-
-
-// var animalGuessCount;
-// var faveAnimalPoints = 0
-// var faveAnimalFromList;
-// var correctAnswers = 0
-// for (animalGuessCount = 6; animalGuessCount > 0; animalGuessCount--) {
-//   var userAnimalGuess = prompt('Please take a guess at one of Lotus\'s favorite animals.\n\nWe currently have ' + animalGuessCount + ' guesses and ' + faveAnimalPoints + ' points!');
-//     if (userAnimalGuess === lotusFaveAnimals[1]) {
-//       alert('Wow ' + userName + '! You got one!\n\nYou have ' + correctAnswers.toString() + ' out of ' + lotusFaveAnimals.length.toString() + '!');
-//     }
-//   }
-//
-// while (animalGuessCount > 0 || correctAnswers === 1) {
-//
-// }
+favoriteAnimals();
 
 
 // alert(userGuessedRoll);
