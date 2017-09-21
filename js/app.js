62baa8b07d675ea432f05d0661ac3a7cf520924c
birdie <birdiedev@gmail.com>
2017-09-20 17:36:21 -0700

    added to the questions and kicked off the number guessing question

--- js/app.js
+++ js/app.js
@@ -2,32 +2,32 @@
 
 // Begin the game!!!
 
-var userName = prompt('Hi there! My name is Birdie. What is your name?');
+var userName = prompt('Hi there! My name is Lotus. What is your name?');
 
-alert('Hi, ' + userName + '! Let\'s play a guessing game! Please guess YES or NO to the following questions...');
+alert('Hi, ' + userName + '! Let\'s play a series of guessing games! For the next five questions please guess YES or NO....');
 
 // Question One - Trombone Question
-var tromboneGuess = prompt('Question 1: Birdie can play the trombone? y/n').toLowerCase();
+var tromboneGuess = prompt('Question 1: Lotus can play the trombone? y/n').toLowerCase();
 console.log('#1 - Trombone question answer is "n" or "no". ' + userName + ' entered ' + tromboneGuess);
 // Trombone question responses to user
 if (tromboneGuess === 'n' || tromboneGuess === 'no') {
-  alert('You are correct! Birdie can\'t play the trombone although she (sort of) can play the clarinet.');
+  alert('You are correct! Lotus can\'t play the trombone although she (sort of) can play the clarinet.');
 } else {
-  alert('You are incorrect. Birdie is honored that you think so but unfortunately she does not play the trombone.');
+  alert('You are incorrect. Lotus is honored that you think so but unfortunately she does not play the trombone.');
 }
 
-// Question Two - Dog Question
-var dogNameGuess = prompt('Question 2: Birdie has a dog named Rosie. y/n').toLowerCase();
-console.log('#2 - Dog question answer is "n" or "no". ' + userName + ' entered ' + dogNameGuess);
+// Question Two - Person Question
+var personNameGuess = prompt('Question 2: Lotus has a person named Ducky. y/n').toLowerCase();
+console.log('#2 - Person question answer is "n" or "no". ' + userName + ' entered ' + personNameGuess);
 // Dog question responses to user
-if (dogNameGuess === 'no' || dogNameGuess === 'n') {
-  alert('Ah, yes! Good job, ' + userName + '! Birdie\'s dog is named Lotus.');
+if (personNameGuess === 'no' || personNameGuess === 'n') {
+  alert('Ah, yes! Good job, ' + userName + '! Lotus\'s person is named Birdie.');
 } else {
-  alert('So close, ' + userName + '! Birdie\'s dog is named after a different flower. The lotus.');
+  alert('So close, ' + userName + '! Lotus\'s person is named after a different animal. The birdie.');
 }
 
 // Question Three - Flight Question
-var flightGuess = prompt('Question 3: Birdie can really fly? y/n').toLowerCase();
+var flightGuess = prompt('Question 3: Lotus can really fly? y/n').toLowerCase();
 console.log('#3 - Flight question answer is "y" or "yes". ' + userName + ' entered ' + flightGuess);
 // Flight question responses to user
 if (flightGuess === 'y' || flightGuess === 'yes') {
@@ -36,15 +36,15 @@ if (flightGuess === 'y' || flightGuess === 'yes') {
   alert('Heh exactly!');
 }
 else {
-  alert('Sorry, wrong answer ' + userName + '. Birdie can indeed fly.');
+  alert('Sorry, wrong answer ' + userName + '. Lotus can indeed fly.');
 }
 
 // Question Four - Unicorn Question
-var unicornGuess = prompt('Question 4: Birdie is part unicorn? y/n').toLowerCase();
+var unicornGuess = prompt('Question 4: Lotus is part unicorn? y/n').toLowerCase();
 console.log('#4 - Unicorn question answer is "n" or "n". ' + userName + ' entered ' + flightGuess);
 // Unicorn question responses to user
 if (unicornGuess === 'y' || unicornGuess === 'yes') {
-  alert('Incorrect. Birdie is not part unicorn. But maybe... It was a good guess, ' + userName + '.');
+  alert('Incorrect. Lotus is not part unicorn. But maybe... It was a good guess, ' + userName + '.');
 } else {
   alert('Yeah, you are right ' + userName + '. It would be cool, though!');
 }
@@ -58,6 +58,24 @@ if (simulationGuess === 'n' || simulationGuess === 'no') {
 } else {
   alert('Whoa, cool! Thanks for the heads up ' + userName + '.');
 }
+
+// Question Six - Dexterity Question (Number guessing game)
+alert('Ok ' + userName + '... now we are going to mix it up! In the next game, we\'ll roll a D20 to do a dexterity check on you. \n\nI\'ll roll the D20 behind the computer so you can\'t see. I need you to guess level (random number between 1 and 20) in FOUR tries or less so we can win!! I know you can do it!');
+var userDexterityRoll = Math.floor(Math.random() * 20) + 1;
+console.log('#6 - Dexterity number guessing game. Number rolled is ' + userDexterityRoll);
+var tries = 4;
+while (tries > 0) {
+  var userGuessedRoll = Number(prompt(userName + ', what is your guess (between 1 - 20)? *fingers crossed* '));
+  if (userGuessedRoll === userDexterityRoll) {
+    alert('OMG we did it! Thank you ' + userName + '!\nWe can now move on to the next game!!!');
+    break;
+  } else {
+    tries--;
+    alert('Oh no! That is not it! Please try again.. We only have ' + tries + ' guesses left!');
+  }
+}
+
+alert(userGuessedRoll);
 
 // Nice goodbye
 alert(userName + '! That was so fun! I hope you enjoyed playing! Have a wonderful day!');
